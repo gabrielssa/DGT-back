458ab7e1d2b80f6360407e83690529c38b3c49b3
Gabriel <gabriel.schubert.araujo@ccc.ufcg.edu.br>
2020-05-28 12:58:36 -0300

    update

--- routes/auth.js
+++ routes/auth.js
@@ -1,23 +1,49 @@
 const router = require('express').Router();
 const User = require('../model/User');
-
-
-
-
+const {registerValidation, loginValidation} = require('../validation');
+const bcrypt = require('bcryptjs');
 
 router.post('/register', async (req, res) => {
+    //Lets validate the data before we a user
+    const {error} = registerValidation(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
     
+    //Checking if the user is already in db
+    const emailExist = await User.findOne({email: req.body.email});
+    if(emailExist) return res.status(400).send('Email já cadastrado');
+
+    //Criptografar o password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    //Create a new user
     const user = new User({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password
+        password: hashedPassword
     })
     try{
         const savedUser = await user.save();
-        res.send(savedUser);
+        res.send({user: user._id});
     }catch(err){
         res.status(400).send(err);
     }
+});
+
+
+//Lógica de login
+router.post('/login', async (req, res) => {
+    //Lets validate the data before we a user
+    const {error} = loginValidation(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+        //Checking if the email exist
+        const user = await User.findOne({email: req.body.email});
+        if(!user) return res.status(400).send('Email não encontrado');
+        //Password is correct
+        const validPass = await bcrypt.compare(req.body.password, user.password);
+        if(!validPass) return res.status(400).send('Invalid Password');
+
+        res.send('Logged in')
 });
 
 module.exports = router;
--- validation.js
+++ validation.js
@@ -2,7 +2,7 @@
 const Joi = require('@hapi/joi');
 
 //Register validation
-const registerValidation = () =>{
+const registerValidation = data =>{
     const schema = Joi.object({
         name: Joi.string().min(6).required(),
         email: Joi.string().min(6).required().email(),
@@ -10,7 +10,18 @@ const registerValidation = () =>{
     });
     //Lets validate the data
     
-    return schema.validate(req.body);
+    return schema.validate(data);
 };
 
+//Login Validation
+const loginValidation = data => {
+    const schema = Joi.object({
+        email: Joi.string().min(6).required().email(),
+        password: Joi.string().min(6).required()
+    });
+
+    return schema.validate(data);
+}
+
 module.exports.registerValidation = registerValidation;
+module.exports.loginValidation = loginValidation;
